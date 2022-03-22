ee60f13a26d3e3a9cc173b3cc20e75b677c2ba6d
tomato_tory <101031776+cutetory@users.noreply.github.com>
2022-03-22 15:50:26 +0900

    namespace연습

---
+++ NameSpacePractice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NameSpace.Utilities;
+
+namespace NameSpace
+{
+    internal class NameSpacePractice
+    {
+        static void Main(string[] args)
+        {
+            string value = "Microsoft Learn";
+            string reversedValue = NameSpace.Utilities.Utility.Reverse(value);
+            Console.WriteLine($"Secret message: {reversedValue}");
+            
+        }
+    }
+}
+
+namespace NameSpace.Utilities
+{
+    class Utility
+    {
+        public static string Reverse(string message)
+        {
+            char[] letters = message.ToCharArray();
+            Array.Reverse(letters);
+            return new string(letters);
+        }
+    }
+}
