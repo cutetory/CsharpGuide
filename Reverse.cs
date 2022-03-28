a203c3698c459a61b93de9bb5a80f46de6e2c906
tomato_tory <101031776+cutetory@users.noreply.github.com>
2022-03-28 17:51:58 +0900

    foreach추가

--- Reverse.cs
+++ Reverse.cs
@@ -23,6 +23,17 @@ namespace reverse
 
             string result = String.Join(" ", newMessage);
             Console.WriteLine(result);
+            
+            string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+            string[] items = orderStream.Split(',');
+
+            foreach (var item in items)
+            {
+                if (item.StartsWith("B"))
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
